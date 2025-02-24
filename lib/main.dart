0fefdd63e2bc92339a9c876fe86ee1f9d9ae7ec0
virti7 <virtipanchamia25@gmail.com>
2025-02-25 00:05:57 +0530

    updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -17,27 +17,56 @@ import 'package:ewaste/presentations/user/home/widgets/didyouknow.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'pages/landing.dart';  // Now the path is correct
+import 'pages/onboarding1.dart';
+import 'pages/onboarding2.dart';
+import 'pages/onboarding3.dart';
+import 'pages/modelwaiting.dart';
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+//   // Manually configure Firebase options
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
 
-  // Manually configure Firebase options
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+//   runApp(const Ewaste());
+// }
 
-  runApp(const Ewaste());
+// class Ewaste extends StatelessWidget {
+//   const Ewaste({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: RegisterPage(),
+//     );
+//   }
+// }
+ 
+ 
+
+
+
+void main() {
+  runApp(const MyApp());
 }
 
-class Ewaste extends StatelessWidget {
-  const Ewaste({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'EcoElectronic',
       debugShowCheckedModeBanner: false,
       home: EnterDetailsPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const SplashScreen(),
     );
   }
 }
- 
